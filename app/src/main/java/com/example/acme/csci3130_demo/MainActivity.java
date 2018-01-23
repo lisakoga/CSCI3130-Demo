2175d027db67784b3c73ac92b05bd2a52d8b5ace
lizzie <ls256469@dal.ca>
2018-01-23 14:35:06 -0400

    added GUI

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.acme.csci3130_demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    /*
+     * The following code is added
+     */
+    public void textbox(View view)
+    {
+        TextView text1 = findViewById(R.id.textView);
+        EditText text2 = findViewById(R.id.editText);
+        text1.setText(text2.getText());
+    }
+
 }
